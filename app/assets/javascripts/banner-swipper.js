9e9e9ab8cb41794a3f9bc0a39fad3b1720fdc736
DmzTheo <dumez.theo.h@gmail.com>
2020-02-06 13:59:40 +0100

    first page

---
+++ app/assets/javascripts/banner-swipper.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper ('.swiper-container', {
+	autoplay: 3500,
+	speed: 1500,
+	speed: 1000,
+	spaceBetween: 30,
+	effect:'fade',
+	centeredSlides: true,
+	watchSlidesVisibility: true,
+	preloadImages: false,
+	lazyLoading: true
+});
