5bc2f1d4a32184ec48672efc0de8d99a65ee6df6
DmzTheo <dumez.theo.h@gmail.com>
2020-03-05 17:02:27 +0100

    autoplay swipper

--- app/assets/javascripts/confiance-swipper.js
+++ app/assets/javascripts/confiance-swipper.js
@@ -2,7 +2,7 @@ var swiper = new Swiper('.swiper-container', {
 	slidesPerView: 'auto',
 	centeredSlides: true,
 	spaceBetween: 30,
-	autoplay: 500,
+	autoplay: 1,
     loop: true,
 	speed: 3100,
 	disableOnInteraction: false,
