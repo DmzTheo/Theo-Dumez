1589748e9fb068071088279128a30634a166542e
DmzTheo <dumez.theo.h@gmail.com>
2020-03-13 16:32:29 +0100

    try 587

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
-    :port => "465",
+    :port => "587",
     :domain => 'gmail.com',
     :user_name => ENV["GMAIL_USERNAME"],
     :password => ENV["GMAIL_PASSWORD"],
