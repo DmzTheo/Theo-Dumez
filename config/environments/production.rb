aefb3aed51f05c6ae225b48a8eddb56fe65aac72
DmzTheo <dumez.theo.h@gmail.com>
2020-03-13 16:20:59 +0100

    try 465

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,12 @@ Rails.application.configure do
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => 'data-web-univers.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'data-web-univers.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'stmp.gmail.com',
-    :port => "587",
+    :port => "465",
     :domain => 'gmail.com',
     :user_name => ENV["GMAIL_USERNAME"],
     :password => ENV["GMAIL_PASSWORD"],
