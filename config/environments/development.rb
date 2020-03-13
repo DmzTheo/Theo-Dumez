5a1f3f285dd7adae9306d0c8ba4ce4b5f9eb075e
DmzTheo <dumez.theo.h@gmail.com>
2020-03-13 14:58:40 +0100

    try gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,11 +44,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.sendgrid.net",
+    :address => 'smtp.gmail.com',
     :port => "587",
-    # :domain => "mydomain.net",
-    :user_name => 'apikey',
-    :password => ENV["SENDGRID_API_KEY"],
+    :domain => 'gmail.com',
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,11 +70,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.sendgrid.net",
+    :address => 'smtp.gmail.com',
     :port => "465",
-    :domain => "data-web-univers.com",
-    :user_name => 'apikey',
-    :password => ENV["SENDGRID_API_KEY"],
+    :domain => 'gmail.com',
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
