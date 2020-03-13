a47400ddbff781d7e8fa7e7487aaa86333c4a76c
DmzTheo <dumez.theo.h@gmail.com>
2020-03-13 12:23:30 +0100

    sendgrid email sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,11 +44,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => "smtp.sendgrid.net",
     :port => "587",
     # :domain => "mydomain.net",
-    :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"],
+    :user_name => 'apikey',
+    :password => ENV["SENDGRID_API_KEY"],
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,11 +70,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => "smtp.sendgrid.net",
     :port => "465",
     # :domain => "mydomain.net",
-    :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"],
+    :user_name => 'apikey',
+    :password => ENV["SENDGRID_API_KEY"],
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
