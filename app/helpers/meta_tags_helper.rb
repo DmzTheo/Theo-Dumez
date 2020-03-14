ba03821dfd2400dc97a55329a9f3ae0ad4af6bea
DmzTheo <dumez.theo.h@gmail.com>
2020-03-14 12:42:00 +0100

    link no follow and noindex méta-tag

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -15,4 +15,8 @@ module MetaTagsHelper
     # avec une image dans vos assets ou une url absolue
     meta_image.starts_with?("http") ? meta_image : image_url(asset_path("Growth-marketing.png"))
   end
+
+  def meta_robots
+    content_for?(:meta_robots) ? content_for(:meta_robots) : DEFAULT_META["meta_robots"]
+  end
 end
