653def4b59f49d7101e090fc87c78ec1fa1347e0
DmzTheo <dumez.theo.h@gmail.com>
2020-03-27 13:40:20 +0100

    recaptcha secret key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
 	config.site_key = ENV["RECAPTCHA_PUBLIC_KEY"]
-	# config.secret_key = ENV["RECAPTCHA_PRIVATE_KEY"]
+	config.secret_key = ENV["RECAPTCHA_PRIVATE_KEY"]
 	# config.site_key = ENV["RECAPTCHA_LOCAL_PUBLIC_KEY"]
 	# config.secret_key = ENV["RECAPTCHA_LOCAL_PRIVATE_KEY"]
 end
