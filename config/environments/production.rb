e23992aff2c65c4ae602d33052c4fd264e17e9a3
DmzTheo <dumez.theo.h@gmail.com>
2020-04-02 12:15:17 +0200

    Leverage browser caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,11 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, s-maxage=#{365.days.to_i}, maxage=#{180.days.to_i}",
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
+
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Theo_Dumez_production"
