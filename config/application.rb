4a45a25a4055cdfc8690f97f4a6fed808094ece8
DmzTheo <dumez.theo.h@gmail.com>
2020-04-02 12:21:53 +0200

    enable compression

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,14 @@ Bundler.require(*Rails.groups)
 module TheoDumez
   class Application < Rails::Application
     config.generators do |generate|
-          generate.assets false
-          generate.helper false
-          generate.test_framework  :test_unit, fixture: false
-        end
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.middleware.use Rack::Deflater
 
     config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
