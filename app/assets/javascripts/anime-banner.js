332a4e34307992a239205111cf55d18ac73ff35e
DmzTheo <dumez.theo.h@gmail.com>
2020-04-02 11:11:17 +0200

    banner animation

--- app/assets/javascripts/anime-banner.js
+++ app/assets/javascripts/anime-banner.js
@@ -4,9 +4,8 @@ anime({
   direction: 'reverse',
   easing: 'easeInOutSine',
   autoplay: true,
-  duration: 1500
+  duration: 1000
 });
-
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml12');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
@@ -19,7 +18,29 @@ anime.timeline({})
     opacity: [0,1],
     easing: "easeOutExpo",
     duration: 1200,
-    delay: (el, i) => 2000 + 30 * i
+    delay: (el, i) => 1000 + 30 * i
+  });
+
+anime.timeline({})
+  .add({
+    targets: '.rotating-text-word',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 1000 + 30 * i
+  });
+
+anime.timeline({})
+  .add({
+    targets: '.rouge-font',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 1000 + 30 * i
   });
 
 // bleu cercle
