0832579e697da1aa6a9bdefac82ce26ac7516cd0
DmzTheo <dumez.theo.h@gmail.com>
2020-04-03 15:14:53 +0200

    add_www_subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+	before_action :add_www_subdomain
 
 	def default_url_options
     	{ host: request.host_with_port }
+	end
+
+	private
+
+	def add_www_subdomain
+		unless /^www/.match(request.host)
+			redirect_to("#{request.protocol}x.com#{request.request_uri}",
+			          :status => 301)
+		end
 	end
 end
