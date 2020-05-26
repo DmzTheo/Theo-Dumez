33e58345f34b9d8f287a32d6ed88faa4be7fe53c
DmzTheo <dumez.theo.h@gmail.com>
2020-05-26 11:42:50 +0200

    policies controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  protect_from_forgery with: :exception
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
